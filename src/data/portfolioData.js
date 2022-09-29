bfa17624697205c8530e2905db3954816e58a44b
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-09-29 18:35:19 -0500

    add more projects

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,19 +1,47 @@
-import booksmart from '../assets/images/1/BookSmart.png'
-import petspace from '../assets/images/1/petspace.png'
+import booksmart from "../assets/images/1/BookSmart.png";
+import petspace from "../assets/images/1/petspace.png";
+import WD from "../assets/images/1/weather-dashboard.png";
+import JATE from "../assets/images/1/jate.png";
+import TB from "../assets/images/1/tb.png";
+import Backend from "../assets/images/1/Backend.png";
 
 export const portfolioData = {
-    "portfolio": [
-        {
-            "cover": booksmart,
-            "title": "BookSmart",
-            "description": "HTML, CSS, JS, jQuery, JSON, Font Awesome",
-            "url": "https://github.com/dannyyyspam/BookSmart"
-        },
-        {
-            "cover": petspace,
-            "title": "PetSpace",
-            "description": "Git, JS, Node, npm, MySQL, API",
-            "url": "https://github.com/dannyyyspam/petSpace/tree/danny"
-        }
-    ]
-}
+  portfolio: [
+    {
+      cover: booksmart,
+      title: "BookSmart 📖",
+      description: "HTML, CSS, JS, jQuery, JSON, Font Awesome",
+      url: "https://github.com/dannyyyspam/BookSmart",
+    },
+    {
+      cover: petspace,
+      title: "🐶 PetSpace 🐱",
+      description: "Git, JS, Node, npm, MySQL, API",
+      url: "https://github.com/dannyyyspam/petSpace/tree/danny",
+    },
+    {
+      cover: WD,
+      title: "Weather Dashboard 🌤",
+      description: "HTML, CSS, BootStrap, jQuery, Moment.js",
+      url: "https://github.com/dannyyyspam/weather-dashboard",
+    },
+    {
+      cover: JATE,
+      title: "Text Editor (J.A.T.E.) ✍",
+      description: "JS, Webpack, IndexedDB, Node.js, Express",
+      url: "https://github.com/dannyyyspam/Text-Editor",
+    },
+    {
+      cover: TB,
+      title: "(MVC) Tech Blog 👋",
+      description: "JS, MySQL, Insomnia, Node.js, npm",
+      url: "https://github.com/dannyyyspam/MVC-Tech-Blog",
+    },
+    {
+      cover: Backend,
+      title: "(ORM) E-Commerce Back End 🗄️",
+      description: "JS, express, sequelize, dotenv MySql",
+      url: "https://github.com/dannyyyspam/petSpace/tree/danny",
+    },
+  ],
+};
